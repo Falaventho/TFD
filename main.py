3978cb0a6508e3c0a5929e6eae84c60cfebabebd
Falaventho <falaventho@gmail.com>
2024-11-16 12:33:59 -0500

    Added tkinter to support ui, updated requirements.txt to match new modules

---
+++ main.py
@@ -0,0 +1,13 @@
+from ui import UserInterface
+import tkinter as tk
+
+
+def main():
+
+    root = tk.Tk()
+    ui = UserInterface(root)
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
--- ui.py
+++ ui.py
@@ -1,8 +1,18 @@
 from dataprocessor import DataProcessor
 from outputgenerator import OutputGenerator
 from utils import Record
+import tkinter as tk
+from tkinter import ttk, messagebox, filedialog
 
 
 class UserInterface:
 
-    pass
+    def __init__(self, root):
+        self.root = root
+        self.data_processor = DataProcessor()
+        self.output_generator = OutputGenerator()
+        self._setup_ui()
+
+    def _setup_ui(self):
+        self.root.title("Investment Projection")
+        self.root.geometry("500x500")
