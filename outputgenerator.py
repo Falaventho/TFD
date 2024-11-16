4e52dae40bf460a268371c81f4f1f4fa3c155bd6
Falaventho <falaventho@gmail.com>
2024-11-16 15:06:06 -0500

    Added output generator method definitions

--- outputgenerator.py
+++ outputgenerator.py
@@ -1,7 +1,16 @@
+from utils import Record
+
+
 class OutputGenerator():
 
     def __init__(self):
         pass
 
-    def generate_projection_csv(self, records: list) -> str:
+    def generate_projection_csv(self, records: list[Record]) -> str:
+        pass
+
+    def generate_projection_html(self, records: list[Record]) -> str:
+        pass
+
+    def generate_error_output(self, errors: list[str]) -> str:
         pass
