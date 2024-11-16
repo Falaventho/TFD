9267a5128848750fc1af5d7396663c1c5a8f278c
Falaventho <falaventho@gmail.com>
2024-11-16 15:11:52 -0500

    Added 'generate_report' entrypoint method alongside 'ReportType' enum for switching and handling of all reports through single interface.

--- outputgenerator.py
+++ outputgenerator.py
@@ -1,9 +1,20 @@
 from utils import Record
+from enum import Enum
+
+
+class ReportType:
+    PROJECTION_CSV = 0
+    PROJECTION_HTML = 1
+    CURRENT_CSV = 10
+    CURRENT_HTML = 11
 
 
 class OutputGenerator():
 
     def __init__(self):
+        pass
+
+    def generate_report(self, records: list[Record], type: ReportType) -> str:
         pass
 
     def generate_projection_csv(self, records: list[Record]) -> str:
